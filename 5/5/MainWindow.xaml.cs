a5bbde87155b8e443a57863d386a93b734d78693
Malka Schildt <malka_88@outlook.com>
2019-03-16 20:23:27 +0700

    5

---
+++ 5/5/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void M1open_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "Document";
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text documents (.txt)|*.txt";
+            dlg.ShowDialog();
+
+            string line;
+
+            System.IO.StreamReader file = new System.IO.StreamReader(dlg.FileName);
+            while ((line = file.ReadLine()) != null)
+            {
+                listBox.Items.Add(line);
+            }
+
+            file.Close();
+        }
+
+        private void M1save_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "Document";
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text documents (.txt)|*.txt";
+            dlg.ShowDialog();
+            //string[] lines = new string[100000];
+
+            List<string> lines = new List<string>();
+
+            using (System.IO.StreamWriter outputFile = new System.IO.StreamWriter(dlg.FileName))
+            {
+                foreach (string line in listBox.Items)
+                    outputFile.WriteLine(line.ToString());
+            }
+
+        }
+    }
+}
